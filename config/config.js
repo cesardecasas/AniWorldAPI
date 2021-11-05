7de8cb48f0dbb28d7c123dc1760f3393a1e3606f
Cesar De Casas <cesar.decasasgarcia@gmail.com>
2021-11-05 09:32:56 -0500

    fixed errors for deployment

---
+++ config/config.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+module.exports = {
+  development: {
+    username: "cesar",
+    password: "Druida78",
+    database: "AniAPI_development",
+    host: "127.0.0.1",
+    dialect: "postgres"
+  },
+  test: {
+    username: "cesar",
+    password: null,
+    database: "AniAPI_test",
+    host: "127.0.0.1",
+    dialect: "postgres"
+  },
+  production: {
+    use_env_variable:'DATABASE_URL',
+    database: 'AniAPI_production',
+    dialect: "postgres"
+  }
+}
+
+
+require('dotenv').config()
+module.exports = {
+  development: {
+    database: '<Your Dev Database>',
+    dialect: 'postgres'
+  },
+  test: {
+    database: '<Your Database Test Name>',
+    dialect: 'postgres'
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+        require: true
+      }
+    }
+  }
+}
